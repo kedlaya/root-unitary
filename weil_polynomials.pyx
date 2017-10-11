388bf5fcb7f91b450d6a07fa37eefcebc9c7ccc7
Kiran S. Kedlaya <kskedL@gmail.com>
2017-10-11 08:54:28 -0700

    First attempt to pass mpz's to C code

--- weil_polynomials.pyx
+++ weil_polynomials.pyx
@@ -8,14 +8,6 @@ AUTHOR:
                    define WeilPolynomials iterator
                    reverse convention for polynomials
         
-EXAMPLES::
-    sage: polRing.<x> = PolynomialRing(Rationals())
-    sage: P0 = polRing([3,5,6,7,5,4,2,-1,-3,-5,-5,-5,-5,-3,-1,2,4,5,7,6,5,3])
-    sage: roots_on_unit_circle(P0, 3^2, 1)
-    [3*x^21 + 5*x^20 + 6*x^19 + 7*x^18 + 5*x^17 + 4*x^16 + 2*x^15 - x^14
-    - 3*x^13 - 5*x^12 - 5*x^11 - 5*x^10 - 5*x^9 - 3*x^8 - x^7 + 2*x^6
-    + 4*x^5 + 5*x^4 + 7*x^3 + 6*x^2 + 5*x + 3]
-
 NOTES:
     This depends on `trac`:23947: for the trace polynomial construction.
 
@@ -36,8 +28,10 @@ from cython.parallel import prange
 from libc.stdlib cimport malloc, free
 cimport cython
 
+from same.rings.integer import Integer
 from sage.rings.rational_field import QQ
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.libs.gmp.types cimport *
 
 cdef extern from "power_sums.h":
     ctypedef struct ps_static_data_t:
@@ -45,7 +39,7 @@ cdef extern from "power_sums.h":
     ctypedef struct ps_dynamic_data_t:
         long count
 
-    ps_static_data_t *ps_static_init(int d, int lead, int sign, int q,
+    ps_static_data_t *ps_static_init(int d, int q, int sign, mpz_t lead,
     		     		     int cofactor, 
                                      int *modlist,
                                      long node_limit)
@@ -76,37 +70,18 @@ cdef class dfs_manager:
     cdef ps_static_data_t *ps_st_data
     cdef ps_dynamic_data_t **dy_data_buf
 
-    def __cinit__(self, int d, int n, int lead, int sign, int q, int cofactor,
-                  int num_threads, modlist, node_limit, Q):
+# Minimal memory allocation.
+    def __cinit__(self, int d, int num_threads):
         cdef int i
         self.d = d
         self.k = d
-        self.sign = sign
-        self.cofactor = cofactor
         self.num_threads = num_threads
         self.Q0_array = array.array('i', [0,] * (d+1))
-        self.Q0 = self.Q0_array
         self.Qsym_array = array.array('i', [0,] * (2*d+3))
-        self.Qsym = self.Qsym_array
         self.modlist_array = array.array('i', [0,] * (d+1))
-        self.modlist = self.modlist_array
-        for i in range(d+1):
-            self.modlist[i] = modlist[d-i]
-            self.Q0[i] = Q[i]
-        if node_limit == None:
-            self.node_limit = -1
-        else:
-            self.node_limit = node_limit
-        self.count = 0
-        self.ps_st_data = ps_static_init(d, lead, sign, q, cofactor,
-                                    self.modlist_array.data.as_ints,
-                                         self.node_limit)
         self.dy_data_buf = <ps_dynamic_data_t **>malloc(num_threads*cython.sizeof(cython.pointer(ps_dynamic_data_t)))
-        self.dy_data_buf[0] = ps_dynamic_init(d, self.Q0_array.data.as_ints)
-        for i in range(1, self.num_threads):
-            self.dy_data_buf[i] = NULL
 
-    def __init__(self, int d, int n, int lead, int sign, int q, int cofactor,
+    def __init__(self, int d, int n, mpz_t lead, int sign, int q, int cofactor,
                  int num_threads, modlist, node_limit, Q):
         pass
 
@@ -118,6 +93,30 @@ cdef class dfs_manager:
                 ps_dynamic_clear(self.dy_data_buf[i])
             free(self.dy_data_buf)
             self.dy_data_buf = NULL
+
+# This method is needed because __cinit__ requires Python calling conventions,
+# and so cannot accept pure C types such as mpz_t. 
+    def populate(self, int n, mpz_t lead, int sign, int q, int cofactor,
+                 int num_threads, modlist, node_limit, Q):
+        self.sign = sign
+        self.cofactor = cofactor
+        self.Q0 = self.Q0_array
+        self.Qsym = self.Qsym_array
+        self.modlist = self.modlist_array
+        for i in range(d+1):
+            self.modlist[i] = modlist[d-i]
+            self.Q0[i] = Q[i]
+        if node_limit == None:
+            self.node_limit = -1
+        else:
+            self.node_limit = node_limit
+        self.count = 0
+        self.ps_st_data = ps_static_init(d, q, sign, lead, cofactor,
+                                    self.modlist_array.data.as_ints,
+                                         self.node_limit)
+        self.dy_data_buf[0] = ps_dynamic_init(d, self.Q0_array.data.as_ints)
+        for i in range(1, self.num_threads):
+            self.dy_data_buf[i] = NULL
 
     cpdef object advance_exhaust(self):
         """
@@ -161,17 +160,44 @@ class WeilPolynomials():
     Iterator for Weil polynomials, i.e., integer polynomials with all complex roots
     having a particular absolute value.
 
-    By default, Weil polynomials are computed using a serial depth-first search.
-    Alternatively, if num_threads is specified, a parallel search (using the specified
-    number of threads) is carried out at creation time.
+    If num_threads is specified, a parallel search (using the specified
+    number of threads) is carried out. In this case, the order of values is not
+    specified.
 
     EXAMPLES:
 
+    By Kronecker's theorem, a monic integer polynomial has all roots of absolute
+    value 1 if and only if it is a product of cyclotomic polynomials. For such a
+    product to have positive sign of the functional equation, the factors `x-1`
+    and `x+1` must each occur with even multiplicity. This code confirms Kronecker's
+    theorem for polynomials of degree 6::
+        sage: P.<x> = PolynomialRing(ZZ)
+        sage: d = 6
+        sage: ans1 = list(WeilPolynomials(d, 1, 1))
+        sage: ans1.sort()
+        sage: l = [(x-1)^2, (x+1)^2] + [cyclotomic_polynomial(n,x) 
+        ....:     for n in range(3, 2*d*d) if euler_phi(n) <= d]
+        sage: w = WeightedIntegerVectors(d, [i.degree() for i in l])
+        sage: ans2 = [prod(l[i]^v[i] for i in range(len(l))) for v in w]
+        sage: ans2.sort()
+        sage: print(ans1 == ans2)
+        True
     """
     def __init__(self, d, q, sign=1, lead=1, P0=None, modlist=None, node_limit=None,
                  num_threads=1):
         r"""
         Initialize an iterator for Weil polynomials.
+
+        INPUT:
+        
+            -- ``d`` - degree of the Weil polynomials
+            -- ``q`` - square absolute value of the roots
+            -- ``sign`` - sign of the functional equation (default: 1)
+            -- ``lead`` - one or more initial coefficients (default: 1)
+            -- ``node_limit`` -- if specified, maximum number of nodes to allow in
+                   a single tree traversal without raising a RuntimeError
+            -- ``num_threads`` -- number of threads to use for parallel computation
+                   (default: 1)
         """
         if P0 is None:
             self.pol = PolynomialRing(QQ, name='x')
@@ -190,9 +216,13 @@ class WeilPolynomials():
             n = 1
             coeffsign = 1
             Q0 = x**d2
+            try:
+                leadlist = list(lead)
+            except TypeError:
+                leadlist = [lead]
             if modlist is None:
-                modlist = []
-            modlist = [0] + [1 for _ in range(d2+1-len(modlist))]
+                modlist = [0 for _ in range(len(lead))]
+            modlist += [1 for _ in range(d2+1-len(modlist))]
         else:
             self.pol = P0.parent()
             x = self.pol.gen()
@@ -216,9 +246,15 @@ class WeilPolynomials():
                 raise StopIteration
             self.ans = self.process.advance_exhaust()
         if len(self.ans) == 0:
+            self.count = self.process.count
             self.process = None
             raise StopIteration
         return self.pol(self.ans.pop())
+
+    def node_count(self):
+        if self.process is None:
+            return self.count
+        return self.process.count
 
 def roots_on_unit_circle(P0, modulus=1, n=1,
                          answer_count=None, node_limit=None, filter=None,
@@ -249,6 +285,13 @@ def roots_on_unit_circle(P0, modulus=1, n=1,
             in order to compute the list.
 
     EXAMPLES::
+        sage: polRing.<x> = PolynomialRing(Rationals())
+        sage: P0 = polRing([3,5,6,7,5,4,2,-1,-3,-5,-5,-5,-5,-3,-1,2,4,5,7,6,5,3])
+        sage: roots_on_unit_circle(P0, 3^2, 1)
+        [3*x^21 + 5*x^20 + 6*x^19 + 7*x^18 + 5*x^17 + 4*x^16 + 2*x^15 - x^14
+        - 3*x^13 - 5*x^12 - 5*x^11 - 5*x^10 - 5*x^9 - 3*x^8 - x^7 + 2*x^6
+        + 4*x^5 + 5*x^4 + 7*x^3 + 6*x^2 + 5*x + 3]
+
         sage: pol.<x> = PolynomialRing(Rationals())
         sage: roots_on_unit_circle(x^5 - 1, 2, 1, return_nodes=True)
         ([x^5 - 1, x^5 - 2*x^4 + 2*x^3 - 2*x^2 + 2*x - 1], 4)
