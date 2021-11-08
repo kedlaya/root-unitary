27fd285010c16c8e635c87cba2d20343e2821670
Kiran Kedlaya <kskedl@gmail.com>
2021-11-08 08:32:07 -0800

    Fix symmetrization, other synchronization with Sage

--- weil_polynomials.pyx
+++ weil_polynomials.pyx
@@ -109,7 +109,7 @@ cdef class dfs_manager:
         cdef fmpz_t temp_lead
         cdef fmpz_t temp_q
         cdef fmpz *temp_array
-        cdef int i = parallel if parallel else 1
+        cdef int i = 101 if parallel else 1
 
         self.d = d
         self.num_processes = i
@@ -308,7 +308,7 @@ class WeilPolynomials_iter():
         # Asymmetrize initial coefficients
         for i in range(len(coefflist)):
             for j in range(1, (len(coefflist)-i+1)//2):
-                coefflist[i+2*j] -= (d2-i).binomial(j)*coefflist[i]
+                coefflist[i+2*j] -= (d2-i).binomial(j)*(q**j)*coefflist[i]
         for _ in range(d2+1-len(coefflist)):
             coefflist.append(0)
             modlist.append(1)
@@ -317,13 +317,8 @@ class WeilPolynomials_iter():
         if node_limit is None:
             node_limit = -1
         force_squarefree = Integer(squarefree)
-        np = self.num_processes
-        if not np.is_prime():
-            np = next_prime(np)
-        while not GF(np)(2).is_primitive_root():
-            np = next_prime(np)
         self.process = dfs_manager(d2, q, coefflist, modlist, coeffsign,
-                                   num_cofactor, node_limit, np,
+                                   num_cofactor, node_limit, parallel,
                                    force_squarefree)
         self.q = q
         self.squarefree = squarefree
@@ -362,6 +357,22 @@ class WeilPolynomials_iter():
                 self.process = None
                 raise StopIteration
         return self.pol(self.ans.pop())
+
+    def next(self): # For Python2 backward compatibility
+        r"""
+        Step the iterator forward.
+
+        Included for Python2 backward compatibility.
+
+        EXAMPLES::
+
+            sage: from sage.rings.polynomial.weil.weil_polynomials import WeilPolynomials
+            sage: w = WeilPolynomials(10,1,sign=1,lead=[3,1,1])
+            sage: it = iter(w)
+            sage: next(it)
+            3*x^10 + x^9 + x^8 + 7*x^7 + 5*x^6 + 2*x^5 + 5*x^4 + 7*x^3 + x^2 + x + 3
+        """
+        return self.__next__()
 
     def node_count(self):
         r"""
@@ -410,22 +421,25 @@ class WeilPolynomials():
 
     - ``sign`` -- integer (default `1`), the sign `s` of the functional equation
 
-    - ``lead`` -- integer, list of integers or list of pairs of integers (default `1`),
-        constraints on the leading few coefficients of the generated polynomials.
+    - ``lead`` -- integer, list of integers or pairs of integers (default `1`)
+
+        These are constraints on the leading coefficients of the generated polynomials.
         If pairs `(a, b)` of integers are given, they are treated as a constraint
         of the form `\equiv a \pmod{b}`; the moduli must be in decreasing order by
         divisibility, and the modulus of the leading coefficient must be 0.
 
-    - ``node_limit`` -- integer (default ``None``), an upper bound on the number of
-        terminal nodes during the search (will raise a ``RuntimeError`` if exceeded)
+    - ``node_limit`` -- integer (default ``None``)
+
+        If set, imposes an upper bound on the number of terminal nodes during the search 
+        (will raise a ``RuntimeError`` if exceeded).
 
     - ``parallel`` -- boolean (default ``False``), whether to use multiple processes
-        in searching for Weil polynomials.  If set, then this file must have been
-        compiled with OpenMP support (see instructions at the top of
-        :mod:`sage.rings.polynomial.weil.weil_polynomials`)
 
-    - ``squarefree`` -- boolean (default ``False``), whether to only include squarefree
-        polynomials in the results
+        If set, will raise an exception unless this file was compiled with OpenMP support.
+
+    - ``squarefree`` -- boolean (default ``False``), 
+
+        If set, only squarefree polynomials will be returned.
 
     - ``polring`` -- optional, a polynomial ring in which to construct the results
 
@@ -504,6 +518,31 @@ class WeilPolynomials():
         True
         True
         True
+
+    Test that :trac:`29475` is resolved::
+
+        sage: P.<x> = QQ[]
+        sage: u = x^6 + x^5 + 6*x^4 - 2*x^3 + 66*x^2 + 121*x + 1331
+        sage: u.is_weil_polynomial()
+        True
+        sage: u in WeilPolynomials(6, 11, 1, [1,1,6])
+        True
+        sage: u in WeilPolynomials(6, 11, 1, [(1,0),(1,11),(6,11)])
+        True
+
+    Test that :trac:`31809` is resolved::
+
+        sage: foo = list(WeilPolynomials(12, 3, lead=(1,0,9,2,46), squarefree=False)) 
+        sage: bar = list(WeilPolynomials(12, 3, lead=(1,0,9,2,46), squarefree=True))
+        sage: bar == [f for f in foo if f.is_squarefree()]                              
+        True
+
+    Test that :trac:`32348` is resolved::
+
+        sage: list(WeilPolynomials(10, 2, lead=(1,-3,5,-5,5,-5)))
+        [x^10 - 3*x^9 + 5*x^8 - 5*x^7 + 5*x^6 - 5*x^5 + 10*x^4 - 20*x^3 + 40*x^2 - 48*x + 32]
+
+
     """
     def __init__(self, d, q, sign=1, lead=1, node_limit=None, parallel=False, squarefree=False, polring=None):
         r"""
