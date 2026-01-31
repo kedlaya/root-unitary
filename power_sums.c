5cad4a3ea88d99b9335777d23ef15eb26ea48d0b
Kiran S. Kedlaya <kskedl@gmail.com>
2026-01-31 10:35:56 -0800

    Update copyright date

--- power_sums.c
+++ power_sums.c
@@ -6,7 +6,7 @@
   TODO: try the Routh-Hurwitz criterion.
 
 #*****************************************************************************
-#       Copyright (C) 2019 Kiran S. Kedlaya <kskedl@gmail.com>
+#       Copyright (C) 2019-2026 Kiran S. Kedlaya <kskedl@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
