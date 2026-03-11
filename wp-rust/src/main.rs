564eed97c84c317f5f738ca2f3be93ac04006801
Kiran S. Kedlaya <kskedl@gmail.com>
2026-03-11 09:54:00 -0700

    Simplify polling loop in Rust

--- wp-rust/src/main.rs
+++ wp-rust/src/main.rs
@@ -1,8 +1,5 @@
 use std::{env, ptr, thread};
 use std::alloc::{alloc_zeroed, dealloc, Layout};
-use std::collections::VecDeque;
-use std::cmp::min;
-use std::iter::zip;
 use std::sync::mpsc::{channel, Sender};
 
 mod bindings;
@@ -41,29 +38,28 @@ fn main() {
     let mut ans_count = 0;
 
     // Initialize static data used by the C code.
-    let st_data = StaticPtr{ptr: unsafe {ps_static_init(d32, q as *const i64, lead as *const i64, ptr::null_mut(), -1, 0) }};
+    let null = ptr::null_mut();
+    let st_data = StaticPtr{ptr: unsafe {ps_static_init(d32, q as *const i64, lead as *const i64, null, -1, 0) }};
     
     // Construct deques for loaded work packets, empty packets, and Senders to dispatch work to threads.
-    let mut work: VecDeque<DynamicPtr> = VecDeque::with_capacity(max_threads);
-    let mut reserve: VecDeque<DynamicPtr> = VecDeque::with_capacity(max_threads);
-    let mut dispatch: VecDeque<Sender<DynamicPtr>> = VecDeque::with_capacity(max_threads);
+    let mut work: Vec<DynamicPtr> = Vec::with_capacity(max_threads);
+    let mut reserve: Vec<DynamicPtr> = Vec::with_capacity(max_threads);
+    let mut dispatch: Vec<Sender<Option<DynamicPtr>>> = Vec::with_capacity(max_threads);
 
     let layout = Layout::array::<i64>((d+1) as usize).unwrap();
     unsafe{
         let ptr = alloc_zeroed(layout) as *mut i64;
         *ptr.add(d as usize) = *lead;
-        work.push_back(DynamicPtr{ptr: ps_dynamic_init(d32, ptr)});
+        work.push(DynamicPtr{ptr: ps_dynamic_init(d32, ptr)});
         dealloc(ptr as *mut u8, layout);
     }
-    for _ in 1..max_threads { reserve.push_back(DynamicPtr{ptr: unsafe { ps_dynamic_init(d32, ptr::null_mut()) }}); }
+    for _ in 1..max_threads { reserve.push(DynamicPtr{ptr: unsafe { ps_dynamic_init(d32, null) }}); }
 
     // Construct channel to return answers.
     let (tx_answers, rx_answers) = channel::<Vec<i64>>();
 
     // Construct channel to return packets.
     let (tx_data, rx_data) = channel::<DynamicPtr>();
-
-    let null = ptr::null_mut();
 
     // Run the outer loop while there is still outstanding work.
     while reserve.len() < max_threads {
@@ -73,12 +69,12 @@ fn main() {
         for data in work.drain(..) {
             let tx_answers_clone = tx_answers.clone();
             let tx_data_clone = tx_data.clone();
-            let (tx_dispatch, rx_dispatch) = channel::<DynamicPtr>();
-            dispatch.push_back(tx_dispatch);
+            let (tx_dispatch, rx_dispatch) = channel::<Option<DynamicPtr>>();
+            dispatch.push(tx_dispatch);
             thread::spawn(move || {
                 let _ = st_data.clone(); // Makes st_data.ptr available in the spawned thread
                 let sympol = unsafe { (*data.ptr).sympol };
-                let data2 = loop {
+                let x = loop {
                     // Step this process forward. If we find a polynomial, return it via a channel.
                     // Otherwise, check exit/split conditions.
                     let flag = unsafe { ps_next_pol(st_data.ptr, data.ptr, steps) };
@@ -86,10 +82,10 @@ fn main() {
                         let iter = (0..d_size).map(|x| unsafe { *sympol.add(x) });
                         tx_answers_clone.send(Vec::from_iter(iter)).unwrap();
                     } else if flag == 0 { break rx_dispatch.recv().unwrap(); }
-                    else if let Ok(data2) = rx_dispatch.try_recv() { break data2; }
+                    else if let Ok(x) = rx_dispatch.try_recv() { break x; }
                 };
                 // Split if necessary, clean up, and return packets.
-                if !data2.ptr.is_null() {
+                if let Some(data2) = x {
                     unsafe { ps_dynamic_split(st_data.ptr, data.ptr, data2.ptr); }
                     tx_data_clone.send(data2).unwrap();
                 }
@@ -100,27 +96,22 @@ fn main() {
 
         // Collect split and terminated processes.
         for data in rx_data.try_iter() {
-            if unsafe { (*data.ptr).flag } == 0 { reserve.push_back(data); }
-            else { work.push_back(data); }
+            if unsafe { (*data.ptr).flag } == 0 { reserve.push(data); }
+            else { work.push(data); }
         }
 
         // Distribute reserve processes to trigger splitting.
-        let n = min(dispatch.len(), reserve.len());
-        if n > 0 {
-           if steps < max_steps { steps += n as i32; } // Recalibrate steps after runup
-           for (tx, data) in zip(dispatch.drain(..n), reserve.drain(..n)) { tx.send(data).unwrap(); }
-           continue;
+        // If reserve gets exhausted, we force one dummy split to avert deadlock.
+        while let Some(tx) = dispatch.pop() {
+            if steps < max_steps { steps += 1; } // Recalibrate steps after runup
+            let data = reserve.pop(); // Do not unwrap! Pass as an option
+            let exhausted = data.is_none(); // Must do this before data is moved
+            tx.send(data).unwrap();
+            if exhausted { break; }
         }
 
         // Collect and count answers.
-        let new_count = rx_answers.try_iter().map(|x| record(x)).count();
-        ans_count += new_count;
-
-        // If there were no answers collected and no work queued, force a dummy split to avert deadlock.
-        // This can only occur in rare cases where all active threads finish roughly at once.
-        if work.len() == 0 && new_count == 0 {
-            let _ = dispatch.pop_front().map(|x| x.send(DynamicPtr{ ptr: null }));
-        }
+        ans_count += rx_answers.try_iter().map(|x| record(x)).count();
     }
 
     // Unblock the answer channel, then collect remaining answers.
